c76646993c84d92b3115a344a839b64c272a78fb
William Gomes <wrgomes1991@gmail.com>
2023-04-10 00:25:41 -0300

    go hello world with prometheus counter metrics

---
+++ Hello-World/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "hello_processed_total",
+		Help: "The total number of processed calls",
+	})
+)
+
+func main() {
+	fmt.Println("Starting hello-world server...")
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/", helloServer)
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+
+	fmt.Println("increasing hello_processed_total counter")
+	opsProcessed.Inc()
+}
